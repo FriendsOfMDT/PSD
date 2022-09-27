0a03a4916a44ba1b7a453917d645ca3c99c21eb4
Mikael Nystrom <deploymentbunny@outlook.com>
2022-09-27 12:20:49 +0200

    Update New-PSDDriverPackage.ps1
    added support for WIM compression

--- Tools/New-PSDDriverPackage.ps1
+++ Tools/New-PSDDriverPackage.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .Synopsis
-    This script creates a driver packages
+    This script creates driver packages from drivers imported in MDT. 
     
 .Description
     This script was written by Johan Arwidmark @jarwidmark and Mikael Nystrom @mikael_nystrom. This script is for the friends of MDT deployment tools 
@@ -16,28 +16,29 @@
           Contact: @Mikael_Nystrom , @jarwidmark
           Primary: @Mikael_Nystrom 
           Created: 2019-05-09
-          Modified: 2020-07-03
+          Modified: 2022-09-25
 
-          Version - 0.0.0 - () - Finalized functional version 1.
-
-
-          # TODO
-          Speed up, remove stuff not needed
-          Condense script, make one flow
-          Use $Env:temp
+          Version - 0.0.0 - () - Finalized functional version 1
+          Version - 0.0.1 - () - Added support for WIM compression, and improved error handling
 
 .EXAMPLE
-	.\New-MDTDriverPackage.ps1 -psDeploymentFolder E:\PSDProduction
+	.\New-MDTDriverPackage.ps1 -psDeploymentFolder E:\PSDProduction -CompressionType WIM
+	.\New-MDTDriverPackage.ps1 -psDeploymentFolder E:\PSDProduction -CompressionType ZIP
 #>
 
 #Requires -RunAsAdministrator
 
 [CmdletBinding()]
 Param(
-    [string]$psDeploymentFolder = "NA"
+    [string]$psDeploymentFolder = "NA",
+	[ValidateSet("ZIP", "WIM")]
+	[string]$CompressionType
 )
 
 $psDeploymentFolder = $psDeploymentFolder.TrimEnd("\")
+
+# Hard Coded Variables
+$PSDriveName = "PSD"
 
 function Start-PSDLog{
 	[CmdletBinding()]
@@ -165,29 +166,37 @@ if((Test-Path -Path $psDeploymentFolder ) -eq $false){
     Exit
 }
 
-# Hard Coded Variables
-$PSDriveName = "PSD"
-
 # Add MDT Snapin
 $mdtDir = $(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Deployment 4' -Name Install_Dir).Install_Dir
 Write-PSDInstallLog -Message "Import MDT PowerShell Module from $mdtDir"
 Import-Module "$($mdtDir)Bin\MicrosoftDeploymentToolkit.psd1"
 
 Write-PSDInstallLog -Message "Create PSdrive using MDTProvider with the name of PSD:"
-$result = New-PSDrive -Name "$PSDriveName" -PSProvider MDTProvider -Root $psDeploymentFolder -ErrorAction SilentlyContinue
+New-PSDrive -Name $PSDriveName -PSProvider MDTProvider -Root $psDeploymentFolder -ErrorAction SilentlyContinue | Out-Null
+if (-not (Get-PSDrive -Name $PSDriveName)){
+    Write-PSDInstallLog -Message "Creating PSdrive $PSDriveName failed."
+    Write-PSDInstallLog -Message "Exiting" -LogLevel 2
+}
 
-$RootDriverPath = "$PSDriveName" + ":\Out-Of-Box Drivers"
-Write-PSDInstallLog -Message "Setting location of drivers to $RootDriverPath"
+$RootDriverPath = $PSDriveName + ":\Out-Of-Box Drivers"
+Write-PSDInstallLog -Message "Driver location is: $RootDriverPath"
 
-
+# Empty the DriverSources folder to make sure we are in sync with Out-Of-Box Drivers
 $DriverSources = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverSources" -Recurse
 foreach($DriverSource in $DriverSources){
     Write-PSDInstallLog -Message "Trying to remove $($DriverSource.fullname)"
-    Remove-Item -Path $DriverSource.fullname -ErrorAction SilentlyContinue -Recurse
+    Remove-Item -Path $DriverSource.fullname -ErrorAction SilentlyContinue -Recurse | Out-Null
 }
 
+# Make sure we could empty the DriverSources folder, abort if not empty
+$DriverSources = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverSources" -Recurse
+If (($DriverSources | Measure-Object).count -gt 0){
+    Write-PSDInstallLog -Message "DriverSources folder cleanup failed. Please do a manual cleanup, and run the script again" -LogLevel 2
+}
+
+# Get a list of all drivers and copy to source folder
 $RootDrivers = Get-ChildItem -Path $RootDriverPath -Recurse
-$AllDrivers = $RootDrivers | Where-Object NodeType -EQ Driver
+$AllDrivers = $RootDrivers | Where-Object NodeType -EQ Driver 
 foreach($Driver in $AllDrivers){
 
     # Determine source folder
@@ -197,48 +206,67 @@ foreach($Driver in $AllDrivers){
     $RemoveMe = "MicrosoftDeploymentToolkit\MDTProvider::$($PSDriveName):\Out-Of-Box Drivers\"
     $DestinationFolderName = ($Driver.PsParentPath).Replace("$RemoveMe","").Replace("\","-")
     
-    # Create package folder
+    # Create source folder for the package
     $DestinationFolderPath = $psDeploymentFolder + "\PSDResources\DriverSources\" + $DestinationFolderName
     if(!(Test-Path -Path $DestinationFolderPath)){
         Write-PSDInstallLog -Message "Creating driver package folder: $DestinationFolderPath"
-        $result = New-Item -Path $DestinationFolderPath -ItemType Directory -Force
+        New-Item -Path $DestinationFolderPath -ItemType Directory -Force | Out-Null
     }
 
-    # Copy source to destionation
+    # Copy source to destination
     $DestinationDriverFolderName = ($SourceFolderPath | Split-Path -Leaf).Replace("_$($Driver.Hash)","")
     New-Item -Path $($DestinationFolderPath + "\" + $($driver.Class) + "\" + $DestinationDriverFolderName) -ItemType Directory -Force | Out-Null
     Copy-Item -Path $SourceFolderPath\* -Destination $($DestinationFolderPath + "\" + $($driver.Class) + "\" + $DestinationDriverFolderName) -Recurse -Force
 }
-Write-PSDInstallLog -Message "Removing old ZIP Archives"
 
-$DriverZIPs = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverPackages" -Recurse
-foreach($DriverZIP in $DriverZIPs){
-    Write-PSDInstallLog -Message "Trying to remove $($DriverZIP.fullname)"
-    Remove-Item -Path $DriverZIP.fullname -ErrorAction SilentlyContinue -Recurse
+# Making sure no temporary archives exist in DriverPackages
+# New packages are created in the root of DriverPackages and then moved their target folder
+Write-PSDInstallLog -Message "Cleanup any old temporary archives in the root of DriverPackages"
+$TempArchives = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverPackages" | Where-Object { $_.Name -match '^*.zip|^*.wim' }
+foreach($Archive in $TempArchives){
+    Write-PSDInstallLog -Message "Trying to remove $($Archive.fullname)"
+    Remove-Item -Path $Archive.fullname -ErrorAction SilentlyContinue | Out-Null
 }
 
-Write-PSDInstallLog -Message "Creating ZIP archives"
+# Set default compresion type to WIM if not specified on the command line
+If ([string]::IsNullOrEmpty($CompressionType)){
+    $CompressionType = "WIM"
+}
+
+# Creating the drivers packages. 
+Write-PSDInstallLog -Message "Creating archives using $CompressionType"
 $DriverFolders = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverSources"
 foreach($DriverFolder in $DriverFolders){
-    $FileName = ($DriverFolder.BaseName).replace(" ","_") + ".zip"
+    $FileName = ($DriverFolder.BaseName).replace(" ","_") + ".$($CompressionType.ToLower())"
     $DestinationFolderPath = $psDeploymentFolder + "\PSDResources\DriverPackages"
+    $DestFile = "$($DestinationFolderPath)" + "\" + $FileName
     Write-PSDInstallLog -Message "Creating $DestinationFolderPath"
-    $Result = New-Item -Path $DestinationFolderPath -ItemType Directory -Force
-    Add-Type -Assembly ‘System.IO.Compression.FileSystem’ -PassThru | Select -First 1 | foreach {
-        $DestFile = "$($DestinationFolderPath)" + "\" + $FileName
-        Write-PSDInstallLog -Message "Creating $DestFile"
-        [IO.Compression.ZIPFile]::CreateFromDirectory("$($DriverFolder.fullname)", $DestFile)
+    New-Item -Path $DestinationFolderPath -ItemType Directory -Force | Out-Null
+
+    If ($CompressionType -eq "WIM"){
+        # Create WIM format archive
+        New-WindowsImage -CapturePath $DriverFolder.fullname -ImagePath $DestFile -Name "Driver Package" | Out-Null
     }
+    Else {
+        # Create ZIP format archive
+        Add-Type -Assembly 'System.IO.Compression.FileSystem' -PassThru | Select -First 1 | ForEach {
+            Write-PSDInstallLog -Message "Creating $DestFile"
+            [IO.Compression.ZIPFile]::CreateFromDirectory("$($DriverFolder.fullname)", $DestFile)
+        }
+    }
+
 }
 
+# Delete the temporary driver sources 
 $DriverSources = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverSources"
 foreach($DriverSource in $DriverSources){
     Write-PSDInstallLog -Message "Trying to remove $($DriverSource.fullname)"
     Remove-Item -Path $DriverSource.fullname -ErrorAction SilentlyContinue -Recurse -Force
 }
 
-$Zips = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverPackages" -Filter *.zip
-foreach($Zip in $Zips){
-    $FolderPath = New-Item -Path "$psDeploymentFolder\PSDResources\DriverPackages\$($Zip.BaseName)" -ItemType Directory -Force
-    Move-Item -Path $($Zip.FullName) -Destination $($FolderPath.FullName) -Force
+# Move the driver package to the correct folder. Existing driver packages will be overwritten
+$Archives = Get-ChildItem -Path "$psDeploymentFolder\PSDResources\DriverPackages" | Where-Object { $_.Name -match '^*.zip|^*.wim' }
+foreach($Archive in $Archives){
+    $FolderPath = New-Item -Path "$psDeploymentFolder\PSDResources\DriverPackages\$($Archive.BaseName)" -ItemType Directory -Force
+    Move-Item -Path $($Archive.FullName) -Destination $($FolderPath.FullName) -Force
 }
